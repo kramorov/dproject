8994e6bdc2a9c963771bbcb2b6210442e5a79307
kramorov <kramorovs@mail.ru>
2026-06-15 17:59:43 +0300

    update ea model_line1

--- electric_actuators/admin/ea_model_line_admin.py
+++ electric_actuators/admin/ea_model_line_admin.py
@@ -183,37 +183,37 @@ def get_parent_field_name(option_instance, parent_class):
                 return field.name
     return None
 
-
-@admin.register(ModelLine)
-class ModelLineAdmin(admin.ModelAdmin):
-    ordering = ['name']
-    # Показать важные поля в списке объектов модели
-    list_display = ('name', 'default_output_type', 'brand')
-
-    fieldsets = (
-        ('Общая информация', {
-            'fields': (
-                ('name', 'default_output_type', 'brand',), 'default_blinker')
-        }),
-        ('Опции', {
-            'fields': (
-                ('default_ip', 'allowed_ip'), ('default_exd', 'allowed_exd'),
-                ('default_body_coating', 'allowed_body_coating'),
-                ('default_temperature', 'allowed_temperature'),
-                ('default_control_unit_installed', 'allowed_control_unit_installed'),)
-        }),
-        ('Конечные, путевые выключатели и датчики момента', {
-            'fields': (
-                ('default_end_switches', 'allowed_end_switches'), ('default_way_switches', 'allowed_way_switches'),
-                ('default_torque_switches', 'allowed_torque_switches'))
-        }),
-        ('Прочее', {
-            'fields': (
-                ('default_hand_wheel', 'allowed_hand_wheel'), ('default_operating_mode', 'allowed_operating_mode'),
-                )
-        }),
-    )
-
+#
+# @admin.register(ModelLine)
+# class ModelLineAdmin(admin.ModelAdmin):
+#     ordering = ['name']
+#     # Показать важные поля в списке объектов модели
+#     list_display = ('name', 'default_output_type', 'brand')
+#
+#     fieldsets = (
+#         ('Общая информация', {
+#             'fields': (
+#                 ('name', 'default_output_type', 'brand',), 'default_blinker')
+#         }),
+#         ('Опции', {
+#             'fields': (
+#                 ('default_ip', 'allowed_ip'), ('default_exd', 'allowed_exd'),
+#                 ('default_body_coating', 'allowed_body_coating'),
+#                 ('default_temperature', 'allowed_temperature'),
+#                 ('default_control_unit_installed', 'allowed_control_unit_installed'),)
+#         }),
+#         ('Конечные, путевые выключатели и датчики момента', {
+#             'fields': (
+#                 ('default_end_switches', 'allowed_end_switches'), ('default_way_switches', 'allowed_way_switches'),
+#                 ('default_torque_switches', 'allowed_torque_switches'))
+#         }),
+#         ('Прочее', {
+#             'fields': (
+#                 ('default_hand_wheel', 'allowed_hand_wheel'), ('default_operating_mode', 'allowed_operating_mode'),
+#                 )
+#         }),
+#     )
+#
 
 class ElectricOperatingModeOptionInline(admin.TabularInline):
     """Inline для режима работы"""
